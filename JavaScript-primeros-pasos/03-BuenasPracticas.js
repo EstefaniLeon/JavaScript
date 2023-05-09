8aee774025f69c7f03c884a7e9d2a3dae75793bc
Estefani <estefanileonosorio2021@outlook.com>
2023-05-09 03:06:21 -0500

    Aprendiendo buenas practicas con variables

---
+++ JavaScript-primeros-pasos/03-BuenasPracticas.js
@@ -0,0 +1,8 @@
+let nombreEmpleado = "José Perez";
+let NombreEmpleado = "Pedro Ramírez";
+
+let edadEmpleado = 40;
+console.log(edadEmpleado);
+
+edadEmpleado = "Cuarenta";
+console.log(edadEmpleado);
