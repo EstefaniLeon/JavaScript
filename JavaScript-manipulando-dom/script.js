d2cb9c9dab0da3b9062592710ea3f24402998f0f
Estefani <estefanileonosorio2021@outlook.com>
2023-05-12 18:20:13 -0500

    Clase 3 completada

--- JavaScript-manipulando-dom/script.js
+++ JavaScript-manipulando-dom/script.js
@@ -1,12 +1,44 @@
-const btn = document.querySelector('[data-form-btn]');
+(() => {
+  const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  console.log(input.value);
-};
+  const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    //backticks
+    const taskContent = document.createElement('div');
 
-console.log(btn);
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
+  };
+
+  //Arrow functions o funciones anonimas
+  btn.addEventListener('click', createTask);
+
+  const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+  };
+  // Immediately invoked function expression IIFE
+  const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+  };
+})();
